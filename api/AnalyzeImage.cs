47db30af1b6d764369609c248d561ba422895e23
Morten Empeño <morten@empeno.dk>
2021-10-08 10:05:48 +0200

    test api

--- api/AnalyzeImage.cs
+++ api/AnalyzeImage.cs
@@ -17,8 +17,9 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            var someObject = new {text = "This is the returned message"};
             
-            return new OkObjectResult("This is the returned message");
+            return new OkObjectResult(someObject);
         }
     }
 }
