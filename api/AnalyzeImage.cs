9073b8cf190a58780893126d664b2f1869d588d7
Morten Empeño <morten@empeno.dk>
2021-10-09 07:42:38 +0200

    connect html to api

--- api/AnalyzeImage.cs
+++ api/AnalyzeImage.cs
@@ -1,12 +1,9 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Company.Function
 {
@@ -17,8 +14,7 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            var someObject = new {text = "This is the returned message"};
-            
+            var someObject = new {text = "This is the returned message"};            
             return new OkObjectResult(someObject);
         }
     }
