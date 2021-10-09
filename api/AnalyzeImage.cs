354729ec5993c76c96a16342607255f72fa15bbe
Morten Empeño <morten@empeno.dk>
2021-10-09 10:25:44 +0200

    Analyse image from function

--- api/AnalyzeImage.cs
+++ api/AnalyzeImage.cs
@@ -4,6 +4,10 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace Company.Function
 {
@@ -11,11 +15,52 @@ namespace Company.Function
     {
         [FunctionName("AnalyzeImage")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log, ExecutionContext context)
         {
-            var someObject = new {text = "This is the returned message"};            
-            return new OkObjectResult(someObject);
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+
+
+            // Add your Computer Vision subscription key and endpoint
+            string subscriptionKey = config["ComputerVisionKey"] ;
+            string endpoint = "https://web-app-with-image-recognition.cognitiveservices.azure.com/";
+
+ 
+            ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+            var analysisResult = await AnalyzeImageUrl(client, "https://cdn.pixabay.com/photo/2020/04/28/18/18/bee-5105751_960_720.jpg");
+
+
+            
+            return new OkObjectResult(analysisResult);
+        }
+
+        
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+            new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+            { Endpoint = endpoint };
+            return client;
+        }
+
+        public static async Task<ImageAnalysis> AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
+
+            return results;
         }
     }
 }
